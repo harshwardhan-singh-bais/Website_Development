4c0ffff14d975e15ee8a01181b6dfc8955af9d8e
CodewithNishchal <nischal100per@gmail.com>
2025-02-22 23:16:18 +0530

    Add some changes to the header

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,5 +1,6 @@
 const navbar = document.querySelector("#siteHead");
 
+
 function vhToPixels(vh) {
     return Math.round(window.innerHeight * (vh / 100));
 }
@@ -8,13 +9,47 @@ const headerHeight = 20;
 
 const pxHeight = vhToPixels(headerHeight);
 
+function setNewHeights() {
+    const institute = document.querySelector(".Institute_dropDown");
+    const Academics = document.querySelector(".Academics_dropDown");
+    const Admissions = document.querySelector(".Admissions_dropDown");
+
+
+    institute.style.top = '9vh';
+    institute.style.left = '7.5vw';
+
+    Academics.style.top = '9vh';
+    Academics.style.left = '7.5vw';
+
+    Admissions.style.top = '9vh';
+    Admissions.style.left = '31vw';
+}
+
+function removeNewHeight() {
+    const institute = document.querySelector(".Institute_dropDown");
+    const Academics = document.querySelector(".Academics_dropDown");
+    const Admissions = document.querySelector(".Admissions_dropDown");
+
+
+    institute.style.top = '19.5vh';
+    institute.style.left = '0vw';
+
+    Academics.style.top = '19.5vh';
+    Academics.style.left = '0vw';
+
+    Admissions.style.top = '19.5vh';
+    Admissions.style.left = '27.5vw';
+}
+
 window.onscroll = () => {
     if (window.scrollY >= pxHeight) {
         // console.log("Detected");
+        setNewHeights();
         navbar.removeAttribute("id");
         navbar.setAttribute("id", "site_head");
     } else {
+        removeNewHeight();
         navbar.removeAttribute("id");
         navbar.setAttribute("id", "siteHead");
     }
-}
+} 
